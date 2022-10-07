96318bfea83264ea9b500c1c2a17bc970f8887c3
Denis <dendel1998@gmail.com>
2022-10-07 19:52:00 +0300

    Сделано 5 задание, начато 6ое

---
+++ finalproject/src/components/ContextMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="modal">
+    <div class="modal__body">
+      <span class="edit">Редактировать</span>
+      <span class="remove">Удалить</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ContextMenu',
+  props: {
+    isVisible: {
+      type: Boolean
+    }
+  },
+  methods: {
+    showContextMenu() {
+      this.$emit('show')
+    },
+    hideContextMenu() {
+      this.$emit('hide')
+    }
+  }
+};
+</script>
+<style lang="scss" scoped></style>
