665f92ae7cd790515a4f0ed7e553fd7300dd2e8c
Denis <dendel1998@gmail.com>
2022-10-14 18:49:05 +0300

    7 задание

---
+++ finalproject/src/tests/TestComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Test Message is {{ message }}
+  </div>
+</template>
+<script>
+export default {
+  name: 'TestComponent',
+  props: {
+    message: String
+  }
+};
+</script>
+<style scoped lang="scss">
+  
+</style>
---
+++ finalproject/src/tests/testForm.test.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils';
+import Form from '../components/Form.vue';
+
+describe('Form tests', () => {
+  test('Category test', () => {
+    const wrapper = mount(Form);
+    debugger
+  });
+});
